54cd971ec4024d58676fd179e616a05a006ebcdb
NDS-0025 <kennykarnama@gmail.com>
2019-05-16 10:05:26 +0700

    Add sanitazion & create main process for program

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,6 +1,8 @@
 package quiz
 
 import (
+	"strings"
+
 	"github.com/kennykarnama/arithmetic_quiz_go/model/simple_question"
 	"github.com/kennykarnama/arithmetic_quiz_go/parserutil"
 )
@@ -28,11 +30,11 @@ func NewQuizFromCsvFile(fileloc string) (*Quiz, error) {
 	for _, record := range records {
 		q := simple_question.QuestionItem{
 			QuestionDescription: record[0],
-			KeyAnswer:           record[1],
+			KeyAnswer:           strings.TrimRight(record[1], "\n"),
 		}
+
 		questions = append(questions, q)
 	}
-
 	newQuiz := Quiz{
 		Questions: questions,
 	}
